348f02957c3e8303d4d2bd5568f0a9a7ca53995a
Izabella Konstanciak <i.konstanciak@gmail.com>
2016-01-02 00:40:10 +0100

    Change validations in project model to pass the test: Project is invalid when name is not unique for developer

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   belongs_to :developer
 
   validates :developer, presence: true
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true#, uniqueness: true
+  validates_uniqueness_of :name, scope: :developer #added validation of the uniuniqueness of name but only for a particular developer
   validates :description, presence: true
 end
