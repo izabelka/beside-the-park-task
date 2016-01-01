12ab044a6f65366227bcfdbe7d336a4ea22701c7
Izabella Konstanciak <i.konstanciak@gmail.com>
2016-01-01 23:27:54 +0100

    Add password_length to user model and set it to minimum value 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 6..128
 end
+# added password_length: 6..128 to make one test pass
